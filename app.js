0b6c750fc9f7289d6ec9d1d3e6c3de7d9cfa1d7d
Adarsh <adarshkumar0278@gmail.com>
2025-08-14 14:39:34 +0530

    registration fuction done

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const userModel = require("./models/user");
+const cookieParser = require('cookie-parser');
+
 
 const app  =  express();
 
 app.set("view engine" , "ejs");
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
+const bcrypt = require('bcrypt');
+
+
+
+const saltRounds = 10;
+
 
 app.get("/" , (req , res) => {
-    res.send("Hello")
-});
-
-app.post("/registration" , (req , res) => {
     res.render("index")
 });
 
 
+app.post("/registration", async (req, res)=>{
+    let {name, email, age, username, password} = req.body;
+    // let user = await userModel.findOne({email})
+    // if(user) return res.status(500).send("You are already LogIn")
+    
+    bcrypt.genSalt(saltRounds, (err,salt)=>{
+        bcrypt.hash(password,salt,(error,hash) =>{
+             userModel.create({name,email,age,username,password:hash})
+        })
+    })
+        
+})
 
-app.listen(6000);
+
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+});
--- models/user.js
+++ models/user.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/loginform");
 
 const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
     name: String,
+    email: String,
     age: Number,
+    username: String,
     password: String
 });
 
